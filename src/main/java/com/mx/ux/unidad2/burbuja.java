1c5c08b4707c6dac6953fe02f9951341f519e65f
Josue <jr5766236@gmail.com>
2025-10-24 07:30:59 -0600

    ordenamiento por insercion

--- src/main/java/com/mx/ux/unidad2/burbuja.java
+++ src/main/java/com/mx/ux/unidad2/burbuja.java
@@ -1,6 +1,6 @@
 package com.mx.ux.unidad2;
 
-public class Burbuja {
+class Burbuja {
     public static void main(String[] args) {
         int[] numeros = {5, 2, 9, 1, 5, 6};
         bubbleSort(numeros);
