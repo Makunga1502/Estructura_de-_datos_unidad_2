c59ea8d3b6da1c3c3a52b9b16fb7add25ae831b1
Josue <jr5766236@gmail.com>
2025-12-09 07:41:49 -0600

    Clase 09/12/2025 (simulador)

---
+++ src/main/java/com/mx/ux/unidad3/simuladorDFS/SimuladorDfs.java
@@ -0,0 +1,45 @@
+package com.mx.ux.unidad3.simuladorDFS;
+
+public class SimuladorDfs {
+    public static void main(String[] args){
+        PilaListaEnlazada pila = new PilaListaEnlazada();
+
+        pila.push("A");
+        System.out.println("Inicio en A");
+        pila.mostrar();
+
+        String actual = pila.pop();
+        System.out.println("POP & Visitar" + actual);
+        pila.mostrar();
+
+        pila.push("B");
+        System.out.println("B (Vecino de A)");
+        pila.mostrar();
+
+        pila.push("C");
+        System.out.println("C (Vecino de A");
+        pila.mostrar();
+
+        actual = pila.pop();
+        System.out.println("POP & Visitar" + actual);
+        pila.mostrar();
+
+        pila.push("D");
+        System.out.println("D (Vecino de C");
+        pila.mostrar();
+
+        actual = pila.pop();
+        System.out.println("POP & Visitar" + actual);
+        pila.mostrar();
+
+        actual = pila.pop();
+        System.out.println("POP & Visitar" + actual);
+        pila.mostrar();
+
+        pila.push("E");
+        System.out.println("E (Vecino de B");
+        pila.mostrar();
+
+    }
+
+}
